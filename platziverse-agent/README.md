88b1b0fb1f3b4ae2e7014da529fe771fd17229cc
rubenfinametrix <rsoler@finametrix.com>
2018-08-07 12:59:34 +0200

    Feature agent impl

---
+++ platziverse-agent/README.md
@@ -0,0 +1,28 @@
+# platziverse-agent
+
+## Usage
+
+```js
+const PlatziverseAgent = require('platziverse-agent')
+
+const agent = new PlatziverseAgent({
+  interval: 2000
+})
+
+agent.connect()
+
+// This agent only
+agent.on('connected')
+agent.on('disconnected')
+agent.on('message')
+
+// Other Agents
+agent.on('agent/connected')
+agent.on('agent/disconnected')
+agent.on('agent/message', payload => {
+  console.log(payload)
+})
+
+setTimeout(() => agent.disconnect(), 20000)
+
+```
--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,11 +1,85 @@
 'use strict'
 
+const debug = require('debug')('platziverse:agent')
+const mqtt = require('mqtt')
+const defaults = require('defaults')
+const uuid = require('uuid')
+
 const EventEmitter = require('events')
 
-class PlatziverseAgent extends EventEmitter {
-  constructor() {
-    super()
+
+const { parsePayload } = require('./utils')
+
+const options = {
+  name: 'untitled',
+  username: 'platzi',
+  interval: 5000,
+  mqtt: {
+    host: 'mqtt://localhost'
   }
+}
+
+class PlatziverseAgent extends EventEmitter {
+  constructor(opts) {
+    super()
+
+    this._options = defaults(opts, options)
+    this._started = false
+    this._timer = null
+    this._client = null
+    this._agentId = null
+  }
+
+  connect() {
+    if (!this._started) {
+      this._started = true
+      const opts = this._options
+      this._client = mqrr.connect(opts.mqtt.host)
+
+      this._client.subscribe('agent/message')
+      this._client.subscribe('agent/connected')
+      this._client.subscribe('agent/disconnected')
+
+      this._client.on('connect', () => {
+        this._agentId = uuid.v4()
+        this.emit('connected', this._agentId)
+        this._timer = setInterval(() => {
+          this.emit('agent/message', 'this is a message')
+        }, opts.interval)
+      })
+
+      this._client.on('error', () => this.disconnect())
+
+      this._client.on('message', (topic, payload) => {
+        payload = parsePayload(payload)
+
+        let broadcast = false
+
+        switch(topic) {
+          case 'agent/connected':
+          case 'agent/disconnected':
+          case 'agent/message':
+            broadcast = payload && payload.agent && payload.agent.uuid !== this._agentId
+            break
+        }
+
+        if (broadcast) {
+          this.emit(topic, payload)
+        }
+
+      })
+
+    }
+  }
+
+  disconnect() {
+    if (this._started) {
+      clearInterval(this._timer)
+      this._started = false
+      this.emit('disconnected')
+    }
+  }
+
 }
 
 module.exports = PlatziverseAgent
