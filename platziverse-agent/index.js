95fd1acaebe5394d569561b745fde589e4abef09
rubenfinametrix <rsoler@finametrix.com>
2018-08-07 13:51:59 +0200

    Feature addMetric & removeMetric

--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -4,6 +4,8 @@ const debug = require('debug')('platziverse:agent')
 const mqtt = require('mqtt')
 const defaults = require('defaults')
 const uuid = require('uuid')
+const os = require('os')
+const util = require('util')
 
 const EventEmitter = require('events')
 
@@ -28,6 +30,15 @@ class PlatziverseAgent extends EventEmitter {
     this._timer = null
     this._client = null
     this._agentId = null
+    this._metrics = new Map()
+  }
+
+  addMetric(type, fn) {
+    this._metrics.set(type, fn)
+  }
+
+  removeMetric(type) {
+    this._metrics.delete(type)
   }
 
   connect() {
@@ -43,8 +54,37 @@ class PlatziverseAgent extends EventEmitter {
       this._client.on('connect', () => {
         this._agentId = uuid.v4()
         this.emit('connected', this._agentId)
-        this._timer = setInterval(() => {
-          this.emit('agent/message', 'this is a message')
+        this._timer = setInterval(async () => {
+          if (this._metrics.size > 0) {
+            let message = {
+              agent: {
+                uuid: this._agentId,
+                username: opts.username,
+                name: opts.name,
+                hostname: os.hostname() || 'localhost',
+                pid: process.pid
+              },
+              metrics: [],
+              timestamp: new Date().getTime()
+            }
+          }
+
+          for (let [metric, fn] of this._metrics) {
+            if (fn.length == 1) {
+              fn = util.promisify(fn)
+            }
+
+            message.metrics.push({
+              type: metric,
+              value: await Promise.resolve(fn())
+            })
+          }
+
+          debug('Sending', message)
+
+          this._client.publish('agent/message', JSON.stringify(message))
+          this.emit('message', message)
+
         }, opts.interval)
       })
 
@@ -76,7 +116,8 @@ class PlatziverseAgent extends EventEmitter {
     if (this._started) {
       clearInterval(this._timer)
       this._started = false
-      this.emit('disconnected')
+      this.emit('disconnected', this._agentId)
+      this._client.end()
     }
   }
 
