00b06bc92306df75cced243b4fc2904ee9409243
rubenfinametrix <rsoler@finametrix.com>
2018-08-07 16:08:48 +0200

    [api] Router

--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -3,10 +3,14 @@
 const http = require('http')
 const chalk = require('chalk')
 const express = require('express')
+
+const api = require('./api')
+
 const port = process.env.PORT || 3000
 const app = express()
-
 const server = http.createServer(app)
+
+app.use('/api', api)
 
 server.listen(port, () => {
   console.log(`${chalk.green('[platziverse-api]')} Server is listening on port ${port}`)
